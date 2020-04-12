e12cd0af43129e1bee7303597f06198d46992775
XikkeN <e.humphreys@tricentis.com>
2020-04-11 21:04:13 -0400

    deployed the app

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
+mongoose.connect("mongodb://user:password1@ds015942.mlab.com:15942/heroku_f8j070k2", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
